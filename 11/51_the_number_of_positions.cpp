df6bfca5a792505341e39057e005fce36ce65b25
mulani-arif <97341143+mulani-arif@users.noreply.github.com>
2022-08-10 15:15:30 +0530

    Create 51_the_number_of_positions.cpp

---
+++ 11/51_the_number_of_positions.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a,b;
+    cin>>n>>a>>b;
+    if(n-a>b)
+    {
+        cout<<b+1<<endl;
+    }
+    else
+    {
+    cout<<n-a<<endl;
+    }
+}
