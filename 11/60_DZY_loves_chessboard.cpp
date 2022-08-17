e94eb24d16c39319a5cb734346aec5a10be556b2
mulani-arif <97341143+mulani-arif@users.noreply.github.com>
2022-08-17 10:45:32 +0530

    Create 60_DZY_loves_chessboard.cpp

---
+++ 11/60_DZY_loves_chessboard.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            char temp;
+            cin>>temp;
+            if(temp=='-') 
+            {
+            cout<<"-";
+            }
+            else if((i^j)&1) cout<<'W';
+            else cout<<"B";
+        }
+        cout<<endl;
+    }
+}
