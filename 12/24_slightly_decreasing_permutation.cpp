6a2cf3b1c8d9447130081b617a334d205461132f
ARIF MULANI <97341143+mulani-arif@users.noreply.github.com>
2022-12-01 15:59:48 +0530

    Create 24_slightly_decreasing_permutation.cpp

---
+++ 12/24_slightly_decreasing_permutation.cpp
@@ -0,0 +1,40 @@
+#pragma GCC optimize ("Ofast")
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vll;
+typedef vector<pair<ll,ll>> vpll;
+typedef map<pll,ll> mpll;
+typedef map<ll,ll> mll;
+#define max3(a,b,c) max(a,max(b,c))
+#define min3(a,b,c) min(a,min(b,c))
+const int M=1e9+7;
+
+void solve()
+{
+  int n,k;
+  cin>>n>>k;
+  while(k--)
+  {
+    cout<<n--<<" ";
+  }
+  for(int i=1;i<=n;i++)
+  {
+    cout<<i<<" ";
+  }
+  cout<<endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+       int t=1;//cin>>t;
+       while(t--) 
+       {
+        solve();
+       }
+    
+}
