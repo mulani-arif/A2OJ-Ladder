3b201c602a5582a0f580d135c628e8143ade044a
ARIF MULANI <97341143+mulani-arif@users.noreply.github.com>
2022-12-06 18:03:59 +0530

    Create 26_fancy_fence.cpp

---
+++ 12/26_fancy_fence.cpp
@@ -0,0 +1,35 @@
+#pragma GCC optimize ("Ofast")
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vll;
+typedef vector<pair<ll,ll>> vpll;
+typedef map<pll,ll> mpll;
+typedef map<ll,ll> mll;
+typedef map<char,ll> mcll;
+#define max3(a,b,c) max(a,max(b,c))
+#define min3(a,b,c) min(a,min(b,c))
+const int M=1e9+7;
+
+void solve()
+{
+  int n;
+  cin>>n;
+  int ext=180-n;
+  if(360%ext) cout<<"NO\n";
+  else cout<<"YES\n";
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+       int t;cin>>t;
+       while(t--) 
+       {
+        solve();
+       }
+    
+}
